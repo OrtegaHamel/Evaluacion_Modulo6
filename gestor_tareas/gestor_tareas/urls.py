e325cd21dc05fc44ddd2341b3bdb068e93ec2c5f
Ortega Hamel <ortega.hamel@gmail.com>
2025-11-09 20:32:15 -0300

    Ajustes en url del home

--- gestor_tareas/gestor_tareas/urls.py
+++ gestor_tareas/gestor_tareas/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 from tareas.forms import CustomAuthenticationForm
+
+# Sobrescribir el formulario de login
+auth_views.LoginView.form_class = CustomAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tareas/', include('tareas.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(pattern_name='login'), name='home'),
 ]
-
-# Sobrescribir el formulario de login
-auth_views.LoginView.form_class = CustomAuthenticationForm
